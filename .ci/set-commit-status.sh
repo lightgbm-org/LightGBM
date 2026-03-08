541c1e31c3ce016f9238e4e18c2968911bce712c
James Lamb <jaylamb20@gmail.com>
2026-03-07 22:14:11 -0600

    update organization reference, copyrights

--- .ci/set-commit-status.sh
+++ .ci/set-commit-status.sh
@@ -51,4 +51,4 @@ curl -sL \
   -H "Accept: application/vnd.github.v3+json" \
   -H "Authorization: token ${GITHUB_TOKEN}" \
   -d "$data" \
-  "${GITHUB_API_URL}/repos/microsoft/LightGBM/statuses/$sha"
+  "${GITHUB_API_URL}/repos/lightgbm-org/LightGBM/statuses/$sha"
